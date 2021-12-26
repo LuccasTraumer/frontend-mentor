41a1ca001d18e60f7c18dbba2d851dcd5bcce6eb
Lucas Jesus <cc19372@g.unicamp.br>
2021-12-26 11:42:24 -0300

    Ajust no carrousel

--- ecommerce-product-page-main/ecomerce/src/app/carrousel/carrousel.component.ts
+++ ecommerce-product-page-main/ecomerce/src/app/carrousel/carrousel.component.ts
@@ -9,6 +9,7 @@ import {Constantes} from "../../assets/constantes";
 export class CarrouselComponent implements OnInit {
   public CONSTANTES = Constantes;
   private index = 0;
+  private isClicked = false;
   public images = [
     this.CONSTANTES.IMAGE_PRODUCT_1, this.CONSTANTES.IMAGE_PRODUCT_2,
     this.CONSTANTES.IMAGE_PRODUCT_3, this.CONSTANTES.IMAGE_PRODUCT_4
@@ -18,9 +19,14 @@ export class CarrouselComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    setInterval(() => {
+      if (!this.isClicked)
+        this.nextImage();
+    }, 4000);
   }
 
   nextImage() {
+    this.isClicked = true;
     this.index++;
     if (this.index > this.images.length -1) {
       this.index = 0;
@@ -40,6 +46,8 @@ export class CarrouselComponent implements OnInit {
 
   changeIndex(index: number) {
     this.index = index;
+
+    setInterval(() => this.isClicked = false, 5000);
   }
 
   getIndex() {
